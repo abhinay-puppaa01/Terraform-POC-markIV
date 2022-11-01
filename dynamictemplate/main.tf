c2f28c535162438e1e16887ccb9957b6f659c9be
abhinay-puppaa01 <110231507+abhinay-puppaa01@users.noreply.github.com>
2022-11-01 15:40:19 +0530

    Update main.tf

--- dynamictemplate/main.tf
+++ dynamictemplate/main.tf
@@ -4,9 +4,5 @@ resource"aws_instance""ec2_test"{
   availability_zone       =var.availability_zone
   subnet_id               =var.subnet_id
   vpc_security_group_ids  =var.vpc_security_group_ids
-  #iam_instance_profile    =var.iam_instance_profile
-  #host_resource_group_arn =var.host_resource_group_arn
-  #tenancy                 =var.tenancy
-  #key_name                =var.key_name
   tags                    =merge({ "Name"=var.instance_name},var.tags)
 }
