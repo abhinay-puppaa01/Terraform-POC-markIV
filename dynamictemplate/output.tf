da2ba91f3973edabd140418927d67117c2dcd01c
abhinay-puppaa01 <110231507+abhinay-puppaa01@users.noreply.github.com>
2022-11-01 15:10:39 +0530

    Update output.tf

--- dynamictemplate/output.tf
+++ dynamictemplate/output.tf
@@ -1,14 +1,14 @@
 output "ec2_id" {
   description = "The ID of the instance"
-  value       = ec2_test.id
+  value = try(aws_instance.ec2_test.id,"")
 }
 
 output "ec2_test_public_ip" {
   description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value       = ec2_test.public_ip
+  value = try(aws_instance.ec2_test.public_ip,"")
 }
 
 output "ec2_test_tags_all" {
   description = "A map of tags assigned to the resource, including those inherited from the provider default_tags configuration block"
-  value       = ec2_test.tags_all
+  value = try(aws_instance.ec2_test.tags,{})
 }
